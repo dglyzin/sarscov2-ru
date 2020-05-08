1f3f598e52e92bd3c1061f4d96ca13661150d251
Dmitry Glyzin <glyzin@gmail.com>
2020-05-08 22:00:59 +0300

    update

---
+++ genutils.py
@@ -0,0 +1,61 @@
+#utilities
+def readgenfile(filename):
+    count = 0 
+    with open(filename,'r') as f:
+        header = ''
+        gotheader = False
+        genomes = []
+        for line in f:
+            #print(line[0])
+            if gotheader:
+                genomes.append({'header':header.rstrip(), 'sequence':line.rstrip()})
+                gotheader = False
+            elif line[0]=='>':
+                header = line[1:]
+                gotheader = True        
+            count += 1
+    print ("lines read: ", count)
+    print ("sequences read: ", len(genomes))
+    return genomes
+
+def savegenfile(filename, genomes):
+    count = 0
+    with open(filename,'w') as f:
+        for genome in genomes:
+            f.write('>'+genome["header"])
+            f.write('\n')
+            f.write(genome["sequence"])
+            f.write('\n')
+            count += 1
+    print('sequences saved:', count)
+
+def filtergenomes(genomes, filter):
+    return [genome for genome in genomes if filter in genome["header"] ]
+
+def filtergensize(genomes, direction, size):
+    if direction=="greater": 
+        return [genome for genome in genomes if len(genome["sequence"])>size ]
+    elif direction=="smaller":
+        return [genome for genome in genomes if len(genome["sequence"])<size ]
+    else:
+        return []
+    
+def getgensampleparams(genomes):
+    '''
+    returns mean and variance of sequence samples length
+    '''
+    glen = len(genomes)
+    if glen>0:
+        total = 0
+        for genome in genomes: 
+            total += len(genome["sequence"])
+        mean = total/glen
+        total = 0
+        for genome in genomes: 
+            diff = len(genome["sequence"])-mean
+            #print(diff*diff)
+            total += diff*diff
+        variance = total/glen
+    else:
+        mean = variance = 0.0
+    return mean, variance
